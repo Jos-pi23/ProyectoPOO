d64f1d3fbb8942e27e2daa5e5cc4145361c994c1
DanielV <danlevil@espol.edu.ec>
2022-12-01 22:36:23 -0500

    Clase Persona terminada.

--- OOPP1/src/main/java/Usuarios/Persona.java
+++ OOPP1/src/main/java/Usuarios/Persona.java
@@ -10,25 +10,28 @@ import java.time.LocalDateTime;
 public class Persona {
     private Scanner entra= new Scanner (System.in);
     private String nombre;
-
     private String email;
-
-    private final int cedula;
-
-    private int telefono;
-
-    public Persona(String nombre, String email, int cedula, int telefono) {
+    private String cedula;
+    private String telefono;
+    
+    private static ArrayList <Persona> personas= new ArrayList();
+    //constructor 1
+    public Persona(){
+    }
+    //constructor 2
+    public Persona(String nombre, String email, String cedula, String telefono) {
         this.nombre = nombre;
         this.email = email;
         this.cedula = cedula;
         this.telefono = telefono;
     }
-
+    
+    //setters y getters
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    protected void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
@@ -36,16 +39,75 @@ public class Persona {
         return email;
     }
 
-    public void setEmail(String email) {
+    protected void setEmail(String email) {
         this.email = email;
     }
 
-    public int getTelefono() {
+    public String getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(int telefono) {
+    protected void setTelefono(String telefono) {
         this.telefono = telefono;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+    public static ArrayList getListaPersonas(){
+        return personas;
+    }
+    ///
+   
+    protected Persona encontrarme(String cedula){
+        return new Persona();
+    }
+    //Metodos privados para el ingreso de datos de un objeto Persona
+    private String definirNombre(){
+//el skip, hay que darle enter para que no se omita el definir nombre. 
+        entra.skip("\n");
+        System.out.println("Ingrese nuevo nombre: ");
+        
+        String nombre2= entra.nextLine();
+        return nombre2;
+    }
+    private String definirEmail(){
+        System.out.println("Ingrese el correo: ");
+        String correo= entra.next();
+        return correo;
+    }
+    private String definirCedula(){
+        System.out.println("Ingrese el numero de la cedula");
+        String cedula= entra.next();
+        return cedula;
+    }
+    private String definirTelefono(){
+        System.out.println("Ingrese el numero de telefono: ");
+        String telefono= entra.next();
+        return telefono;
+    }
+    //Definir datos. metodo sobreecrito por las subclases de Persona
+    public Persona definirDatos(){
+        String nombre=definirNombre();
+        String correo= definirEmail();
+        String cedula= definirCedula();
+        String telefono= definirTelefono();
+        return new Persona(nombre,correo,cedula,telefono);
+    }
+    //Modificar datos usa los setters y getters para cambiar los valores
+    public void modificarDatos(){
+        System.out.println("presione enter para continuar");
+        setNombre(definirNombre());
+        setEmail(definirEmail());
+        setTelefono(definirTelefono());
+    }
+    //metodo que será heredado para cambiar el estado a inactivo.
+    public void eliminarme(){ 
+    }
+    @Override
+    public String toString() {
+        return "nombre= " + nombre + ", email= " + email + 
+        ", cedula= " + cedula + ", telefono= " + telefono + '}';
     }
     
     
